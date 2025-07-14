c044e961548d3354f4a527f968641f40508f4e48
nadunmA <naduncode404@outlook.com>
2025-07-14 07:58:38 +0530

    issue fix

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { ViteImageOptimize } from 'vite-plugin-imagemin'
+
 
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
     tailwindcss(),
-    ViteImageOptimize({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        quality: [0.6, 0.8],
-        speed: 4,
-      },
-      webp: {
-        quality: 80,
-      }
-    })
+    
   ],
   optimizeDeps: {
     include: ['prop-types'],
